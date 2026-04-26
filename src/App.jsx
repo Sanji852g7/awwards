3f3e1a6959b7829a908ce5d0d950759649eb291c
Sanji Gurung <sanji.gurung@hotmail.com>
2026-04-26 22:19:52 +0100

    add my font to css file

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome to Awwards</h1>
+      <h1 className="text-5xl text-orange-500 font-bold ">Welcome to Awwards</h1>
     </main>
   )
-}
+} 
 
 export default App
 
