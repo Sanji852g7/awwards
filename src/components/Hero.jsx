a09ab822f95ded22e6d87b85b13372dfb5355e19
Sanji Gurung <sanji.gurung@hotmail.com>
2026-05-03 00:49:53 +0100

    add hero title and create button component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import { useState, useRef } from 'react';
+import { TiLocationArrow } from 'react-icons/ti';
+import Button from './Button';
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -28,18 +30,20 @@ const Hero = () => {
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
+
         <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
           <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
-            <video 
-            ref={nextVdRef}
-            src={getVideoSrc(upcomingVideoIndex) }
-            loop
-            muted
-            id="current-video"
-            className="size-64 origin-center scale-150 object-cover object-center"
-            onLoadedData={handleVideoLoad}/>
+            <video
+              ref={nextVdRef}
+              src={getVideoSrc(upcomingVideoIndex)}
+              loop
+              muted
+              id="current-video"
+              className="size-64 origin-center scale-150 object-cover object-center"
+              onLoadedData={handleVideoLoad} />
           </div>
         </div>
+
         <video
           ref={nextVdRef}
           src={getVideoSrc(currentIndex)}
@@ -54,13 +58,40 @@ const Hero = () => {
           src={getVideoSrc(
             currentIndex === totalVideos - 1 ? 1 : currentIndex
           )}
-          autoPlay
+          //autoPlay
           loop
           muted
           className="absolute left-0 top-0 size-full object-cover object-center"
           onLoadedData={handleVideoLoad}
         />
       </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+        G<b>A</b>MING
+      </h1>
+
+      <div className="absolute left-0 top-0 z-40 size-full pointer-events-none">
+        <div className="mt-24 px-5 sm:px-10">
+          <h1 className="special-font hero-heading text-blue-100">
+            redefi<b>n</b>e
+          </h1>
+
+          <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+            Enter the Metagame Layer <br /> Unleash the Play Economy
+          </p>
+
+          <Button
+            id="watch-trailer"
+            title="Watch trailer"
+            leftIcon={<TiLocationArrow />}
+            containerClass="bg-yellow-300 flex-center gap-1 pointer-events-auto"
+          />
+        </div>
+      </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>A</b>MING
+      </h1>
     </div>
   );
 };
